1b78669be75588dc0c5f5c0fd71882cde4e495d4
Hanna Hudzen <annguden@gmail.com>
2024-04-03 22:04:09 +0300

    Module 5/1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,55 @@
 export default class NotificationMessage {
+  element;
 
+  static lastInstance;
+
+  constructor(message = '',
+              notificationConfig = {}) {
+    const {duration = 2000, type = 'success' } = notificationConfig;
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.element = this.createNotificationElement(this.createNotificationTemplate());
+  }
+
+  show(targetElement = document.body) {
+    if (NotificationMessage.lastInstance) {
+      NotificationMessage.lastInstance.destroy();
+    }
+    NotificationMessage.lastInstance = this;
+    targetElement.appendChild(this.element);
+
+    this.timerId = setTimeout(() => {
+      this.destroy();
+    }, this.duration);
+  };
+
+  createNotificationElement(template) {
+    const element = document.createElement('div');
+    element.innerHTML = template;
+    return element.firstElementChild;
+  }
+
+  createNotificationTemplate() {
+    return (
+      `<div class="notification ${this.type}" style="--value:${this.duration}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>`
+    )
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    clearTimeout();
+  };
 }
